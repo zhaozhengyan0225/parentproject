6d94ad63c0b847b1fe1ee9a5ad5f9dbf764fd9bd
super_bugfei <super_bugfei@163.com>
2018-06-26 00:43:22 +0800

    一个小整理

--- babasport_console/src/main/java/cn/jiang/core/controller/UploadController.java
+++ babasport_console/src/main/java/cn/jiang/core/controller/UploadController.java
@@ -2,7 +2,6 @@ package cn.jiang.core.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
